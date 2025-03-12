609c1c695e50019bff9062ae17517df030a2e553
stas200268 <simonlip4@gmail.com>
2025-03-12 13:55:04 +0200

    2nd

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Technologies from './Technologies';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
-    </div>
-  );
-}
-
-const Technologies = () => {
-  return (
-    <div className="Technologies">
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
-    </div>
-  );
-}
-
-const Header = () => {
-  return (
-    <div className="Header">
-      <a href="https://www.google.com">Home</a>
-      <a href="https://www.google.com">News Feed</a>
-      <a href="https://www.google.com">Messages</a>
-      <a href="https://www.google.com">Google</a>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className="Header">
+            <a href="https://www.google.com">Home</a>
+            <a href="https://www.google.com">News Feed</a>
+            <a href="https://www.google.com">Messages</a>
+            <a href="https://www.google.com">Google</a>
+        </div>
+    );
+}
+
+export default Header;
